210fbce4900a01b386fee873ce5b6e0e6641651c
gescheit <gescheit@yandex-team.ru>
2015-08-11 21:17:33 +0300

    support ifelse and multicharacter quotes
    +py3
    speedup
    +ifdef

--- m4.py
+++ m4.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-import itertools
 import sys
+from string import Template
+from collections import defaultdict, deque
 
-from collections import defaultdict
+
+class DlrTemplate(Template):
+    delimiter = '$'
+    idpattern = r'[0-9]+'
 
 
 class ParseError(Exception):
@@ -47,26 +51,30 @@ def name(x):
 class peek_insert_iter:
     def __init__(self, iter):
         self.iter = iter
-        self.inserted = []
-        self.peeked = []
+        self.inserted = deque()
+        self.peeked = deque()
 
     def __iter__(self):
         return self
 
-    def next(self):
+    def __next__(self):
         if self.inserted:
-            return self.inserted.pop(0)
+            return self.inserted.popleft()
         if self.peeked:
-            return self.peeked.pop(0)
-        return self.iter.next()
+            return self.peeked.popleft()
+        return next(self.iter)
+
+    next = __next__
 
     def insert(self, iterable):
-        self.inserted[0:0] = iterable
+        iterable = list(iterable)
+        iterable.reverse()
+        self.inserted.extendleft(iterable)
 
     def _peek(self):
         if not self.peeked:
             try:
-                self.peeked.append(self.iter.next())
+                self.peeked.append(next(self.iter))
             except StopIteration:
                 pass
 
@@ -88,6 +96,7 @@ class Lexer:
         self.start_quote = ['`']
         self.end_quote = ["'"]
         self.iter = None
+        self.quote_buffer = deque(maxlen=len(self.start_quote[0]))
 
     def _finish_token(self, name):
         t = Token(name, ''.join(self.chars))
@@ -98,8 +107,16 @@ class Lexer:
         self.iter.insert(text)
 
     def changequote(self, start_quote='`', end_quote='\''):
-        self.start_quote = [start_quote]
-        self.end_quote = [end_quote]
+        self.start_quote = list(start_quote)
+        self.end_quote = list(end_quote)
+        if len(start_quote) != self.quote_buffer.maxlen:
+            self.quote_buffer = deque(self.quote_buffer, maxlen=len(start_quote))
+
+    def ifelse(self, string1, string2, equal, notequal=None):
+        if string1 == string2 and equal:
+            return equal
+        elif string1 != string2 and notequal:
+            return notequal
 
     def parse(self):
         '''
@@ -116,8 +133,9 @@ class Lexer:
             def __iter__(self):
                 return self.iter
 
-            def next(self):
-                return self.iter.next()
+            def __next__(self):
+                return next(self.iter)
+            next = __next__
 
             def peek_char(self):
                 return lexer.iter.peek()
@@ -145,23 +163,35 @@ class Lexer:
 
     def _generic(self, c):
         if c is not EOF:
-            self.chars.append(self.iter.next())
+            self.chars.append(next(self.iter))
+        self.quote_buffer.append(c)
         if c.isalpha() or c == '_':
             self.state = self._identifier
         elif c == '#':
             self.state = self._comment
-        # TODO: handle multi-character quotes
+
+        d = len(self.start_quote) - len(self.chars)
+        borrow = []
+        if d > 0:
+            for _ in range(d):
+                b = next(self.iter)
+                self.chars.append(b)
+                borrow.append(b)
         if self.chars == self.start_quote:
             self.state = self._string
             self.nesting_level = 1
+
         if self.state is None:
+            for _ in borrow:
+                b = self.chars.pop()
+                self.insert_text(b)
             tokens = [Token(c, c) for c in self.chars]
             self.chars = []
             return tokens
         return []
 
     def _string(self, c):
-        self.chars.append(self.iter.next())
+        self.chars.append(next(self.iter))
         if (
                 self.start_quote != self.end_quote and
                 endswith(self.chars, self.start_quote)
@@ -182,12 +212,12 @@ class Lexer:
             self.state = None
             return [self._finish_token('IDENTIFIER')]
 
-        self.chars.append(self.iter.next())
+        self.chars.append(next(self.iter))
         return []
 
     def _comment(self, c):
         if c != '\n' and c is not EOF:
-            self.chars.append(self.iter.next())
+            self.chars.append(next(self.iter))
             return []
 
         self.state = None
@@ -195,8 +225,10 @@ class Lexer:
 
 
 def substmacro(name, body, args):
-    # TODO: implement argument substitution
-    return body
+    kwargs = {str(x + 1): args[x] for x in range(len(args))}
+    kwargs["0"] = name
+    res = DlrTemplate(body).safe_substitute(kwargs)
+    return res
 
 
 class Parser:
@@ -206,6 +238,8 @@ class Parser:
             'dnl': self._builtin_dnl,
             'changequote': self._builtin_changequote,
             'divert': self._builtin_divert,
+            'ifelse': self._builtin_ifelse,
+            'ifdef': self._builtin_ifdef,
         }
         self.lexer = Lexer(text)
         self.token_iter = self.lexer.parse()
@@ -228,6 +262,24 @@ class Parser:
         self.changequote(*args[:2])
         return None
 
+    def _builtin_ifelse(self, args):
+        return self.ifelse(*args)
+
+    def _builtin_ifdef(self, args):
+        if len(args) == 3:
+            macro, string1, string2 = args
+        elif len(args) == 2:
+            macro, string1 = args
+            string2 = None
+        else:
+            raise Exception()
+
+        if macro in self.macros and string1:
+            self.lexer.insert_text(string1)
+        elif macro not in self.macros and string2:
+            self.lexer.insert_text(string2)
+        return None
+
     def _builtin_divert(self, args):
         args = args or [0]
         try:
@@ -243,7 +295,7 @@ class Parser:
         current_arg = []
         if self.token_iter.peek_char() == '(':
             # drop that token
-            tok = self.token_iter.next()
+            tok = next(self.token_iter)
             if tok.value != '(':
                 raise ParseError('Expected open parenthesis but got %s'
                                  % tok.value)
@@ -270,7 +322,8 @@ class Parser:
                     and tok.type == 'IDENTIFIER'
                     and tok.value in self.macros
             ):
-                result = self.macros[tok.value](self._parse_args())
+                args = self._parse_args()
+                result = self.macros[tok.value](args)
                 if result:
                     self.lexer.insert_text(result)
             else:
@@ -281,6 +334,21 @@ class Parser:
 
     def changequote(self, start_quote='`', end_quote='\''):
         self.lexer.changequote(start_quote, end_quote)
+
+    def ifelse(self, *args):
+        if len(args) == 3:
+            return self.lexer.ifelse(args[0], args[1], args[2])
+        elif len(args) == 4:
+            return self.lexer.ifelse(args[0], args[1], args[2], args[3])
+        elif len(args) > 4:
+            arg_chunks = [list(args[i:i + 3]) for i in range(0, len(args), 3)]
+            if len(arg_chunks[-1]) == 1:
+                arg_chunks[-2].append(arg_chunks[-1][0])
+                arg_chunks.pop(-1)
+            for arg_chunk in arg_chunks:
+                res = self.lexer.ifelse(arg_chunk[0], arg_chunk[1], arg_chunk[2])
+                if res:
+                    return res
 
     def parse(self, stream=sys.stdout):
         for tok in self._expand_tokens():
